4c06c9b65dba15b0403ec7a830148fefd5ab518d
Равиль <rav_taibov@mail.ru>
2025-03-11 06:55:00 +0500

    Добавлены новые тесты и документация

---
+++ src/test/java/agon/LoginTest.java
@@ -0,0 +1,53 @@
+package agon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+    @Test
+    public void validLogin() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://agonagon.ru/");
+
+        // Находим и кликаем по кнопке "Войти"
+        WebElement loginButton = driver.findElement(By.linkText("Войти"));
+        loginButton.click();
+
+        // Вводим логин и пароль
+        driver.findElement(By.id("login")).sendKeys("validUser");
+        driver.findElement(By.id("password")).sendKeys("validPassword");
+        driver.findElement(By.id("submit")).click();
+
+        // Проверяем, что вошли в аккаунт
+        WebElement profile = driver.findElement(By.id("profile"));
+        Assertions.assertNotNull(profile, "Профиль не найден!");
+
+        driver.quit();
+    }
+
+    @Test
+    public void invalidLogin() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://agonagon.ru/");
+
+        WebElement loginButton = driver.findElement(By.linkText("Войти"));
+        loginButton.click();
+
+        driver.findElement(By.id("login")).sendKeys("invalidUser");
+        driver.findElement(By.id("password")).sendKeys("invalidPassword");
+        driver.findElement(By.id("submit")).click();
+
+        WebElement error = driver.findElement(By.id("error-message"));
+        Assertions.assertNotNull(error, "Ошибка не показана!");
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/agon/SearchTest.java
@@ -0,0 +1,28 @@
+package agon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchTest {
+
+    @Test
+    public void checkSearch() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://agonagon.ru/");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("стейк");
+        searchBox.submit();
+
+        WebElement result = driver.findElement(By.className("search-result"));
+        Assertions.assertNotNull(result, "Результаты поиска не найдены!");
+
+        driver.quit();
+    }
+}
